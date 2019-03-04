5e5eef587d0b2d0bf59782ef911f49325ebe108a
习常 <chen.cpx@antfin.com>
2019-03-04 16:41:36 +0800

    feat: 兼容 workflow 处理

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -18,12 +18,17 @@ Successful writing
     name: flags.string({char: 'n', description: 'name to print'}),
     // flag with no value (-f, --force)
     force: flags.boolean({char: 'f'}),
+    type: flags.string({char: 't', description: 'type'}),
+    stop: flags.boolean({char: 's'}),
   }
 
-  static args = [{name: 'project'}]
+  static args = [
+    {name: 'project'}
+  ]
 
   async run() {
-    const {args} = this.parse(New)
+    const {args, flags} = this.parse(New)
+    const datetime = dayjs().format('YYYY-MM-DD HH:mm:ss')
     // 2015-06-01 event "location" "New York, USA"
     // 获取用户配置 数据中的 repo
     const configFilePath = path.join(this.config.configDir, 'config.json')
@@ -36,8 +41,14 @@ Successful writing
     // 判断是否有 今日 格式为 YYYY-mm-dd 的数据文件
     const filePath = path.join(repoDir, dayjs().format('YYYY-MM-DD') + '.csv')
     await fs.ensureFile(filePath)
+    // 为了适配 what did i doy.alfredworkflow
+    const project = args.project.includes(' ') ? args.project.split(' ')[0] : args.project
+    const projectType = args.project.includes('--type=') ? args.project.split('--type=')[1] : flags.type
     // const fileData = await fs.readJSON(filePath, {throws: false})
-    const writeData = `${dayjs().format('YYYY-MM-DD HH:mm:ss')},${args.project}\n`
+    let writeData = `${datetime},${projectType || 'work'},${project}\n`
+    if (flags.stop) {
+      writeData = `${datetime},stop,null\n`
+    }
     // 数据文件尾部写入 将要进行的项目 args.project
     await fs.appendFile(filePath, writeData)
     this.log(`Successful writing ${writeData}`)
